b31ef0f94667b046d6a57c7bfc7eabeabb6a5c8b
Se <jose.abril4@gmail.com>
2021-03-17 19:02:34 +0800

    Removed uunused codes

--- src/components/SideMenu/sideMenu.js
+++ src/components/SideMenu/sideMenu.js
@@ -3,10 +3,6 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import DashboardIcon from '@material-ui/icons/Dashboard';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import PeopleIcon from '@material-ui/icons/People';
-import BarChartIcon from '@material-ui/icons/BarChart';
-import LayersIcon from '@material-ui/icons/Layers';
 
 export const sideMenu = (
   <div>
